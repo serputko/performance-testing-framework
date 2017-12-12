cbe5df562697dc99596f2b79133ad23e352b4210
Anton Serputko <antse@ciklum.com>
2017-12-12 18:26:04 +0200

    Added build examples for jmeter, jenkins Added restart option for containers Changed exposed port from 4000 to 80 for webpagetestserver Added new jenkins plugins

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash -x ./bin/jmeter.sh -n -t ${SCENARIO} -l ${WORKSPACE}/report.log -e -o ${WORKSPACE}/report/ -JUSERS=${USERS} -JPAMPUP=${RAMPUP} -JHOLDLOAD=${HOLDLOAD} -JSHUTDOWN=${SHUTDOWN}
+bash ./bin/jmeter.sh -n -t ${SCENARIO} -l ${WORKSPACE}/report.log -e -o ${WORKSPACE}/report/ -JUSERS=${USERS} -JPAMPUP=${RAMPUP} -JHOLDLOAD=${HOLDLOAD} -JSHUTDOWN=${SHUTDOWN}
