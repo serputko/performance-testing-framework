e5ea5d82b6d3f3259bb2041bc05cd9dec6110b90
Anton Serputko <antse@ciklum.com>
2017-12-29 16:40:31 +0200

    - set jmeter log saving to jenkins workspace /var/jenkins_home/workspace/ - change PAMPUP to RAMPUP in jmeter job in jenkins - add name to telegraf host in docker config file - add jmeter docker container stop after build is aborted: - add postbuild-task to jenkins for stopping jmeter docker containerin case of build abort - add throughput to composite graph on grafana dashboard - Add jenkins pre build step for stopping jmeter contains - add hostname: telegraf-loadgenerator to telegraf docker file - add jenkins-home volume, influxdb home and grafana home - change jmeter download link to http://www-eu.apache.org/dist//jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz - add parameter-separator jenkins plugin - separate load generator server metrics and other servers

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash ./bin/jmeter.sh -n -t ${SCENARIO} -l ${WORKSPACE}/report.log -e -o ${WORKSPACE}/report/ -JUSERS=${USERS} -JPAMPUP=${RAMPUP} -JHOLDLOAD=${HOLDLOAD} -JSHUTDOWN=${SHUTDOWN}
+bash ./bin/jmeter.sh -n -t ${SCENARIO} -j ${WORKSPACE}/jmeter.log -l ${WORKSPACE}/report.log -e -o ${WORKSPACE}/report/ -JUSERS=${USERS} -JRAMPUP=${RAMPUP} -JDURATION=${DURATION}
